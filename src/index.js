ea9df801b11527100251eada0c19d7e481d91fdb
Recomagnesius <buzhor.danyl@gmail.com>
2021-03-10 14:48:59 +0200

    feat: fail

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 module.exports = function toReadable (number) {
-  
+    function twoTen(num) {
+        let list = [
+            'zero',
+            'one',
+            'two',
+            'three',
+            'four',
+            'five',
+            'six',
+            'seven',
+            'eight',
+            'nine',
+            'ten',
+            'eleven',
+            'twelve',
+            'thirteen',
+            'fourteen',
+            'fifteen',
+            'sixteen',
+            'seventeen',
+            'eighteen',
+            'nineteen',
+            'twenty',
+        ];
+        if ((number.length == 1 || number.length == 2) && number <= 20) {
+            return list[number];
+        }
+    }
+
+    let list2 = [
+        empty,
+        empty,
+        'twenty',
+        'thirty',
+        'fourty',
+        'fifty',
+        'sixty',
+        'seventy',
+        'eighty',
+        'ninety',
+    ];
+    if (number.length == 2) {
+        number = String(number);
+        number.split();
+        number.forEach(function (item, index, array) {
+            Number(number[index]);
+        });
+        let p1 = twoTen(number[0]);
+        let p2 = list2[number[1]];
+        let result;
+        result = p2 + ' ' + p1;
+        return result;
+    }
 }
